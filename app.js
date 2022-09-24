ac2bb548de8e7f3170de0b650a361102b1db6a12
natchaaws <natchaa62@nu.ac.th>
2022-09-25 03:06:52 +0700

    upload

--- app.js
+++ app.js
@@ -2,14 +2,17 @@
 const cors = require('cors')
 const app = express()
 
-
 app.use(cors())
 app.use(express.json())
 
 //API
 require('./router/router')(app)
 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 //port
-app.listen(3333, function () {
-  console.log('CORS-enabled web server listening on port 3333')
+const port = process.env.PORT || 3333
+app.listen(port, function () {
+  console.log(`CORS-enabled web server listening on ${port}`)
 })
